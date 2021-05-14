9e2320f01b7a0e95580c1de2fd7b6b7f675173d2
Daisy <ramired2@oregonstate.edu>
2021-05-13 19:00:00 -0700

    added cases for pytest bc wouldnt allow input()

--- inClass5/palindrome.py
+++ inClass5/palindrome.py
@@ -1,9 +1,20 @@
 def palin ():
     msg = "Please enter a string: "
     str1 = input(msg)
+
+    # str3 = "apples are fun to eat"
+    # str1 = str(str3)
+
+    # str1 = ""
+    # str3 = "redivider"
+    # str1 = str(str3)
+
     str2 = str1[::-1]
 
-    if not str1.isalpha():
+    if str1 == "":
+        return ""
+
+    if not str2.isalpha():
         return -1
 
     if str1 == str2:
