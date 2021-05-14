86d2f7614eaf4d6af69215a55daba2baae37a185
Daisy <ramired2@oregonstate.edu>
2021-05-13 19:09:33 -0700

    included cases for pytest bc not allow input()

--- inClass5/wordcount.py
+++ inClass5/wordcount.py
@@ -2,4 +2,8 @@ def count ():
     msg = "Please enter a sentence: "
     sntnce = input(msg)
 
+    # sntnce = "fooooooooooooooooo u rrrrrrrrrrrrrrrrrrrrrrrrrrr"
+    # sntnce = "re-read re-read"
+    # sntnce = ""
+
     return len(sntnce.split())
